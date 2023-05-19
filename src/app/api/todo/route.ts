883a2273fdb4d15fca2fa08a446bdbea07e9eeda
RAO FAHAD <raofahad046@gmail.com>
2023-05-20 00:36:06 +0500

    built connection with database and created a table in db

--- src/app/api/todo/route.ts
+++ src/app/api/todo/route.ts
@@ -1,6 +1,15 @@
-// import { db } from "@vercel/postgres";
+import { db } from "@vercel/postgres";
 import { NextRequest, NextResponse } from "next/server";
 
-export function GET(request: NextRequest) {
+export async function GET(request: NextRequest) {
+  const client = await db.connect();
+
+  try {
+    await client.sql`CREATE TABLE IF NOT EXISTS Todos(id SERIAL, Task varchar(255));`;
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ message: "Something Went Wrong" });
+  }
+
   return NextResponse.json({ message: "api is running" });
 }
