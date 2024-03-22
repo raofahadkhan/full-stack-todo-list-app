f09cb06230fea0de2519eb75cc43a8cf68487fc9
Rao Fahad <raofahad046@gmail.com>
2024-03-22 19:56:15 +0500

    added the custom styles to scroll bar

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -3,6 +3,29 @@ import BASE_PATH from "@/lib/basepath";
 import { Todo } from "@/lib/drizzle";
 import DeleteTodo from "./DeleteTodo";
 
+// Custom styles for the scrollbar:
+const customScrollbarStyles = `
+  /* Width */
+  ::-webkit-scrollbar {
+    width: 5px;
+  }
+
+  /* Track */
+  ::-webkit-scrollbar-track {
+    background: transparent;
+  }
+
+  /* Handle */
+  ::-webkit-scrollbar-thumb {
+    background: #888;
+  }
+
+  /* Handle on hover */
+  ::-webkit-scrollbar-thumb:hover {
+    background: #555;
+  }
+`;
+
 const getData = async () => {
   try {
     const res = await fetch(`${BASE_PATH}/api/todo`, {
@@ -22,26 +45,29 @@ const TodoList = async () => {
   const res: { data: Todo[] } = await getData();
 
   return (
-    <div className="h-[450px] overflow-y-auto mb-4">
-      {res.data.length === 0 ? (
-        <h3 className="flex items-center mx-auto">No Todos Found!</h3>
-      ) : (
-        res.data.reverse().map((item) => {
-          return (
-            <div
-              key={item.id}
-              className="bg-gray-100 py-4 px-4 flex items-center justify-between  shadow rounded-lg my-3"
-            >
-              <div className="flex items-center gap-x-3">
-                <div className="h-3 w-3 bg-secondary rounded-full"></div>
-                <p className="text-lg font-medium">{item.task}</p>
+    <>
+      <style>{customScrollbarStyles}</style>
+      <div className="h-[450px] overflow-y-auto mb-4">
+        {res.data.length === 0 ? (
+          <h3 className="flex items-center mx-auto">No Todos Found!</h3>
+        ) : (
+          res.data.reverse().map((item) => {
+            return (
+              <div
+                key={item.id}
+                className="bg-gray-100 py-4 px-4 flex items-center justify-between  shadow rounded-lg my-3"
+              >
+                <div className="flex items-center gap-x-3">
+                  <div className="h-3 w-3 bg-secondary rounded-full"></div>
+                  <p className="text-lg font-medium">{item.task}</p>
+                </div>
+                <DeleteTodo todoId={item.id} />
               </div>
-              <DeleteTodo todoId={item.id} />
-            </div>
-          );
-        })
-      )}
-    </div>
+            );
+          })
+        )}
+      </div>
+    </>
   );
 };
 export default TodoList;
