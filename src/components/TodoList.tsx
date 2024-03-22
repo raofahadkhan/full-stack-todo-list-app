9e9c3b07344764f08d9e6c05dc5fda2af606a49f
Rao Fahad <raofahad046@gmail.com>
2024-03-22 19:31:45 +0500

    updated the add todo functionality

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -24,7 +24,7 @@ const TodoList = async () => {
   return (
     <div className="max-h-[450px] overflow-scroll mb-4">
       {res &&
-        res.data.map((item) => {
+        res.data.reverse().map((item) => {
           return (
             <div
               key={item.id}
